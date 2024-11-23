8b118a56fa6ab1868f8b4ffd97442e05b5209b83
Steven <sirpepega777@gmail.com>
2024-11-22 22:14:58 -0500

    eliminación de credenciales y creación de splash screen y apk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA_8ug7yZggG3mP9B-wbYtk8oa9m9Ur6-o",
-    authDomain: "b-steven.firebaseapp.com",
-    projectId: "b-steven",
-    storageBucket: "b-steven.firebasestorage.app",
-    messagingSenderId: "153829575850",
-    appId: "1:153829575850:web:ce11c0a4f3cc0c9b5694d3",
-    measurementId: "G-V7MXNE52JV"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
